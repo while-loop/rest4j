b31cc70ea4fc8f83a4b04e8b4c910e14f6f54f43
Anthony Alves <cvballa3g0@gmail.com>
2018-07-24 17:58:55 -0400

    add helper json functions to httpresp and httpreq

--- src/main/java/com/github/whileloop/rest4j/HttpRequest.java
+++ src/main/java/com/github/whileloop/rest4j/HttpRequest.java
@@ -1,24 +1,21 @@
 package com.github.whileloop.rest4j;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Map;
-import java.util.TreeMap;
-
-import static com.github.whileloop.rest4j.HttpMethod.GET;
 
 /**
  * Created by aalves on 12/18/17
  */
 public interface HttpRequest extends HttpContext {
     JsonParser parser = new JsonParser();
+    Gson GSON = new Gson();
+
     /**
      * If getBody or getRawBody has already been called, this stream will be empty
      *
@@ -34,7 +31,11 @@ public interface HttpRequest extends HttpContext {
 
     String getProtocol();
 
-    default JsonElement bodyAsJson(){
-        return parser.parse(new InputStreamReader(getRawBody())).getAsJsonObject();
+    default JsonElement bodyAsJson() {
+        return parser.parse(new InputStreamReader(getRawBody()));
+    }
+
+    default <T> T asObject(Class<T> clazz) {
+        return GSON.fromJson(new InputStreamReader(getRawBody()), clazz);
     }
 }
